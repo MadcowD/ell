a890745e9477ac4f81c5b8217843fd735620252d
William Guss <carlostnb@gmail.com>
2024-09-11 23:50:42 -0700

    updated docs

--- examples/claude.py
+++ examples/claude.py
@@ -1,6 +1,6 @@
 import ell  # type: ignore
 
-@ell.simple(model="claude-3-5-sonnet-20240620", max_tokens=12)
+@ell.simple(model="claude-3-5-sonnet-20240620", max_tokens=100)
 def hello_from_claude():
     """You are an AI assistant. Your task is to respond to the user's message with a friendly greeting."""
     return "Say hello to the world!!!"
