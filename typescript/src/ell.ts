f79a3ad11052dbba50ba851e046be1e3a6facb25
Alex Dixon <alex.dixon@embeddedinsurance.com>
2024-09-14 11:52:44 -0700

    get lmp source, name at runtime

--- typescript/src/ell.ts
+++ typescript/src/ell.ts
@@ -1,11 +1,17 @@
-import { callsites } from "./callsites";
+import * as sourceMapSupport from "source-map-support";
+sourceMapSupport.install();
 
-type Kwargs = Record<"name" | "model", string>;
+import { callsites } from "./callsites";
+import { EllTSC } from "./tsc";
+
+type Kwargs = { name: string; model: string; [key: string]: any };
 type F = (...args: any[]) => Promise<any>;
 
 type StackEntry = {
-  name: string;
-  filename: string;
+  name?: string | null;
+  filepath?: string | null;
+  line?: number | null;
+  column?: number | null;
 };
 
 let stack: StackEntry[] = [];
@@ -18,16 +24,46 @@ type Invocation = {
   output: any;
 };
 type LMP = {
-  name: string;
-  filename: string;
-  sourceCode: string;
+  name: string | null;
+  filepath: string | null;
+  line: number | null;
+  column: number | null;
+  sourceCode: string | null;
 };
 
+let tsc = new EllTSC();
 export let invocations: Invocation[] = [];
 export let lmps: LMP[] = [];
 
+let lmpsCache: Record<string, LMP> = {};
+
+const cacheKey = (filename: string, name: string) => `${filename}-${name}`;
+const getLMP = async (filename: string, name: string) => {
+  const key = cacheKey(filename, name);
+  if (lmpsCache[key]) return lmpsCache[key];
+  const lmps = await tsc.getLMPsInFile(filename);
+  for (const lmp of lmps) {
+    // FIXME. we need to pick the name in the config or the var name which could be different
+    // const lmpName = JSON.parse(lmp.config).name
+    // const k = cacheKey(filename,lmpName)
+    // @ts-ignore
+    // lmpsCache[k] = lmp;
+  }
+  const lmp = lmpsCache[key];
+  // todo
+  if (!lmp) {
+    throw new Error(`LMP ${name} not found in file ${filename}`);
+  }
+  return lmp;
+};
+
 const enter = (args: StackEntry) =>
-  stack.push({ name: args.name, filename: args.filename });
+  stack.push({
+    name: args.name,
+    filepath: args.filepath,
+    line: args.line,
+    column: args.column,
+  });
 
 const exit = () => stack.pop();
 
@@ -39,26 +75,79 @@ const writeLMP = async (lmp: LMP) => {
   lmps.push(lmp);
 };
 
-function getCallerFile() {
-  const callerFile = callsites()[2].getFileName();
-  return callerFile ? callerFile : "unknown";
+function getCallerFileLocation() {
+  const callerSite = callsites()[2];
+  let file = callerSite.getFileName();
+  let line = callerSite.getLineNumber();
+  let column = callerSite.getColumnNumber();
+  if (file && line && column) {
+      const mappedPosition = sourceMapSupport.mapSourcePosition({
+        source: file,
+        line: line,
+        column: column,
+      });
+      file = mappedPosition.source;
+      line = mappedPosition.line;
+      column = mappedPosition.column;
+  }
+  return { filepath: file, line, column };
 }
-const serializeLMP = async (args: { name: string; filename: string }) => {
+
+const serializeLMP = async (args: {
+  name: string;
+  filepath: string | null;
+  line: number | null;
+  column: number | null;
+}) => {
   const sourceCode = "";
   return await writeLMP({
     name: args.name,
-    filename: args.filename,
+    filepath: args.filepath,
+    line: args.line,
+    column: args.column,
     sourceCode: sourceCode,
   });
 };
 
+const getLMPName = async (args: {
+  filepath: string | null;
+  line: number | null;
+  column: number | null;
+}) => {
+  return "";
+  // todo
+  // const sourceCode = tsc.getFunctionSource(
+  //   args.filepath,
+  //   args.line,
+  //   args.column
+  // )
+};
+
 export const simple = (a: Kwargs, f: F) => {
-  const name = a.name;
-  const filename = getCallerFile();
+  const { filepath, line, column } = getCallerFileLocation();
+
+  console.log("line", { filepath, line, column });
+
+  if (!filepath || !line || !column) {
+    console.error(
+      `LMP cannot be tracked. Your source maps may be incorrect or unavailable.`
+    );
+    return f;
+  }
 
   const wrapper = async (...args: any[]) => {
-    enter({ name, filename });
-    await serializeLMP({ name, filename });
+    const lmp = await tsc.getLMP(filepath!, line!, column!);
+    if (!lmp) {
+      console.error(
+        `No LMP found at ${filepath}:${line}:${column}. Your source maps may be incorrect or unavailable.`
+      );
+      return f;
+    }
+    console.log("lmp at runtime", lmp);
+    const name = lmp.lmpName;
+    enter({ name, filepath, line, column });
+    // const lmp = await getLMP(filename,name)
+    await serializeLMP({ name, filepath, line, column });
     const result = await f(...args);
     await writeInvocation({
       lmpType: "simple",
@@ -77,9 +166,9 @@ export const complex = (a: Kwargs, f: F) => {
   const name = a.name;
 
   const wrapper = async (...args: any[]) => {
-    const filename = __filename;
-    enter({ name, filename });
-    await serializeLMP({ name, filename });
+    const { filepath, line, column } = getCallerFileLocation();
+    enter({ name, filepath, line, column });
+    await serializeLMP({ name, filepath, line, column });
     const result = await f(...args);
     await writeInvocation({
       lmpType: "complex",
--- typescript/src/tsc.ts
+++ typescript/src/tsc.ts
@@ -1,19 +1,20 @@
 import * as path from "path";
 import ts from "typescript";
 
-
-
 const LMP_FUNCTION_EXPORT_NAMES = ["simple", "complex"];
-const ELL_MODULE_IDENTIFIER = "./ell" || "ell-ai";
-
-
+const ELL_MODULE_IDENTIFIERS = ["ell-ai", "./ell", "../src/ell"];
+const isEllModuleIdentifier = (identifier: string) =>
+  // TODO. remove this once we have a stable import
+  identifier.includes("ell") || ELL_MODULE_IDENTIFIERS.includes(identifier);
 
 /**
  * Get all import declarations in a source file
- * @param sourceFile 
- * @returns 
+ * @param sourceFile
+ * @returns
  */
-const getAllImportDeclarations = (sourceFile: ts.SourceFile): ts.ImportDeclaration[] => {
+const getAllImportDeclarations = (
+  sourceFile: ts.SourceFile
+): ts.ImportDeclaration[] => {
   const importDeclarations: ts.ImportDeclaration[] = [];
 
   const visit = (statements: ts.Statement[] | ts.NodeArray<ts.Statement>) => {
@@ -29,15 +30,19 @@ const getAllImportDeclarations = (sourceFile: ts.SourceFile): ts.ImportDeclarati
 
 /**
  * True if the source file imports `ell-ai`
- * @param sourceFile 
- * @returns 
+ * @param sourceFile
+ * @returns
  */
 const hasEllImport = (sourceFile: ts.SourceFile): boolean => {
   const importDecls = getAllImportDeclarations(sourceFile);
-  return importDecls.some((decl) => ts.isStringLiteral(decl.moduleSpecifier) && decl.moduleSpecifier.text === ELL_MODULE_IDENTIFIER);
+  return importDecls.some(
+    (decl) =>
+      ts.isStringLiteral(decl.moduleSpecifier) &&
+      isEllModuleIdentifier(decl.moduleSpecifier.text)
+  );
 };
 
-interface EllTSC {
+export interface EllTSC {
   getAST(filePath: string): Promise<ts.SourceFile | undefined>;
   getLMPsInFile(filePath: string): Promise<LMP[]>;
   getFunctionSource(
@@ -48,10 +53,23 @@ interface EllTSC {
 }
 
 type LMPType = "simple" | "complex";
-type LMP = { lmpType: LMPType; config: string; fn: string };
+type LMP = {
+  lmpType: LMPType;
+  lmpName: string;
+  config: string;
+  source: string;
+  fn: string;
+  filepath: string;
+  line: number;
+  column: number;
+  endLine: number;
+  endColumn: number;
+};
 
-class EllTSC implements EllTSC {
+export class EllTSC implements EllTSC {
   private program: ts.Program;
+  // maps filePath to LMPs
+  private lmpCache: Map<string, LMP[]> = new Map();
   constructor(rootDir: string = process.cwd()) {
     const configPath = ts.findConfigFile(
       rootDir,
@@ -76,11 +94,68 @@ class EllTSC implements EllTSC {
     });
   }
 
+  private getNodeAtPosition(
+    sourceFile: ts.SourceFile,
+    line: number,
+    column: number
+  ): ts.Node | undefined {
+    const position = ts.getPositionOfLineAndCharacter(
+      sourceFile,
+      line - 1,
+      column - 1
+    );
+
+    let foundNode: ts.Node | undefined;
+
+    function visit(node: ts.Node) {
+      if (node.getStart() <= position && position < node.getEnd()) {
+        foundNode = node;
+        ts.forEachChild(node, visit);
+      }
+    }
+
+    ts.forEachChild(sourceFile, visit);
+
+    return foundNode;
+  }
+
   async getAST(filePath: string): Promise<ts.SourceFile | undefined> {
     return this.program.getSourceFile(filePath);
   }
+  async getLMP(
+    filePath: string,
+    line: number,
+    column: number
+  ): Promise<LMP | null> {
+    let lmp: LMP | null = null;
+    if (this.lmpCache.has(filePath)) {
+      const lmps = this.lmpCache.get(filePath) as LMP[];
+      // for now, match on line only
+      // @ts-ignore
+      lmp = lmps.find((lmp) => lmp.line === line);
+      if (!lmp && lmps.length > 0) {
+        // fall back to using our line start and endings
+
+      }
+    }
+    if (!lmp) {
+      const lmps = await this.getLMPsInFile(filePath);
+      lmp = lmps.find((lmp) => lmp.line === line) || null;
+    }
+    if (!lmp) {
+      console.error(`LMP not found for ${filePath}:${line}:${column}`);
+      const lmpsInFile = this.lmpCache.get(filePath);
+      if (lmpsInFile && lmpsInFile.length > 0) {
+        console.log("LMPs in file", lmpsInFile);
+      }
+    }
+    return lmp;
+  }
 
   async getLMPsInFile(filePath: string): Promise<LMP[]> {
+    if (this.lmpCache.has(filePath)) {
+      return this.lmpCache.get(filePath)!;
+    }
     const sourceFile = await this.getAST(filePath);
     if (!sourceFile) {
       console.log(
@@ -92,7 +167,7 @@ class EllTSC implements EllTSC {
 
     // Fast path: no ell import
     if (!hasEllImport(sourceFile)) {
-      return []
+      return [];
     }
 
     // The symbol `myEll` in any of:
@@ -114,80 +189,118 @@ class EllTSC implements EllTSC {
       complex: "complex",
     };
 
+    function visitImportDeclaration(node: ts.ImportDeclaration) {
+      if (
+        ts.isStringLiteral(node.moduleSpecifier) &&
+        isEllModuleIdentifier(node.moduleSpecifier.text)
+      ) {
+        if (node.importClause) {
+          if (node.importClause.name) {
+            // Default import
+            ellModuleImportIdentifier = node.importClause.name.text;
+          } else if (node.importClause.namedBindings) {
+            if (ts.isNamedImports(node.importClause.namedBindings)) {
+              // Named imports
+              node.importClause.namedBindings.elements.forEach((element) => {
+                // We're using an alias bound to 'name' for something
+                if (element.propertyName) {
+                  if (element.propertyName.text === "default") {
+                    // default import
+                    ellModuleImportIdentifier = element.name.text;
+                  } else if (
+                    // If it's one of the lmp functions
+                    LMP_FUNCTION_EXPORT_NAMES.includes(
+                      element.propertyName.text
+                    )
+                  ) {
+                    // Store the alias for future use
+                    const alias = element.name.text;
+                    const lmpType = element.propertyName.text as LMPType;
+                    lmpTypeToAlias[lmpType] = alias;
+                    aliasToLMPType[alias] = lmpType;
+                  }
+                }
+              });
+            } else if (ts.isNamespaceImport(node.importClause.namedBindings)) {
+              // Namespace import
+              // todo. match on property access
+              // import * as ell from "ell-ai";
+              // ell.simple
+              ellModuleImportIdentifier =
+                node.importClause.namedBindings.name.text;
+            }
+          }
+        }
+      }
+    }
+    function visitCallExpression(node: ts.CallExpression) {
+      // Bare function call
+      // mySimpleAlias()
+      // myComplexAlias()
+      if (ts.isIdentifier(node.expression)) {
+        if (Object.keys(aliasToLMPType).includes(node.expression.text)) {
+          if (node.arguments.length > 0) {
+            const lmpFnConfig = node.arguments[0];
+            const lmpFn = node.arguments[1];
+            return {
+              lmpType: aliasToLMPType[node.expression.text],
+              config: lmpFnConfig.getText(sourceFile),
+              fn: lmpFn.getText(sourceFile),
+            };
+          }
+        }
+      }
+      // Method/property access
+      // myEll.mySimpleAlias()
+      if (
+        ts.isPropertyAccessExpression(node.expression) &&
+        Object.keys(aliasToLMPType).includes(node.expression.name.text) &&
+        ts.isIdentifier(node.expression.expression) &&
+        node.expression.expression.text === ellModuleImportIdentifier
+      ) {
+        if (node.arguments.length > 0) {
+          return {
+            lmpType: aliasToLMPType[node.expression.name.text],
+            config: node.arguments[0].getText(sourceFile),
+            fn: node.arguments[1].getText(sourceFile),
+          };
+        }
+      }
+    }
+
     function visit(node: ts.Node) {
       if (ts.isImportDeclaration(node)) {
-        if (
-          ts.isStringLiteral(node.moduleSpecifier) &&
-          node.moduleSpecifier.text === ELL_MODULE_IDENTIFIER
-        ) {
-          if (node.importClause) {
-            if (node.importClause.name) {
-              // Default import
-              ellModuleImportIdentifier = node.importClause.name.text;
-            } else if (node.importClause.namedBindings) {
-              if (ts.isNamedImports(node.importClause.namedBindings)) {
-                // Named imports
-                node.importClause.namedBindings.elements.forEach((element) => {
-                  // We're using an alias bound to 'name' for something
-                  if (element.propertyName) {
-                    if (element.propertyName.text === "default") {
-                      // default import
-                      ellModuleImportIdentifier = element.name.text;
-                    } else if (
-                      // If it's one of the lmp functions
-                      LMP_FUNCTION_EXPORT_NAMES.includes( element.propertyName.text)
-                    ) {
-                      // Store the alias for future use
-                      const alias = element.name.text;
-                      const lmpType = element.propertyName.text as LMPType;
-                      lmpTypeToAlias[lmpType] = alias;
-                      aliasToLMPType[alias] = lmpType;
-                    }
-                  }
+        visitImportDeclaration(node);
+        // } else if (ts.isCallExpression(node)) {
+      } else if (ts.isVariableStatement(node)) {
+        const declaration = node.declarationList.declarations[0];
+        if (ts.isVariableDeclaration(declaration) && declaration.initializer) {
+          if (ts.isCallExpression(declaration.initializer)) {
+            const lmp = visitCallExpression(declaration.initializer);
+            if (lmp) {
+              if (ts.isIdentifier(declaration.name)) {
+                const lmpName = declaration.name.text;
+                const { line, character } =
+                  sourceFile!.getLineAndCharacterOfPosition(
+                    node.getStart(sourceFile)
+                  );
+                const { line: endLine, character: endCharacter } = sourceFile!.getLineAndCharacterOfPosition(node.getEnd());
+
+                lmps.push({
+                  lmpType: lmp.lmpType,
+                  config: lmp.config,
+                  fn: lmp.fn,
+                  lmpName,
+                  source: node.getText(sourceFile),
+                  filepath: filePath,
+                  // Add 1 because TypeScript uses 0-based line numbers
+                  line: line + 1, 
+                  column: character + 1,
+                  endLine: endLine + 1,
+                  endColumn: endCharacter + 1,
                 });
-              } else if (
-                ts.isNamespaceImport(node.importClause.namedBindings)
-              ) {
-                // Namespace import
-                // todo. match on property access
-                // import * as ell from "ell-ai";
-                // ell.simple
-                ellModuleImportIdentifier =
-                  node.importClause.namedBindings.name.text;
               }
             }
-          }
-        }
-      } else if (ts.isCallExpression(node)) {
-        // Bare function call
-        // mySimpleAlias()
-        // myComplexAlias()
-        if (ts.isIdentifier(node.expression)) {
-          if (Object.keys(aliasToLMPType).includes(node.expression.text)) {
-            if (node.arguments.length > 0) {
-              const lmpFnConfig = node.arguments[0];
-              const lmpFn = node.arguments[1];
-              lmps.push({
-                lmpType: aliasToLMPType[node.expression.text],
-                config: lmpFnConfig.getText(sourceFile),
-                fn: lmpFn.getText(sourceFile),
-              });
-            }
-          }
-        }
-        // Method/property access 
-        // myEll.mySimpleAlias()
-        if (
-          ts.isPropertyAccessExpression(node.expression) &&
-          Object.keys(aliasToLMPType).includes(node.expression.name.text) &&
-          node.expression.expression.getText() === ellModuleImportIdentifier
-        ) {
-          if (node.arguments.length > 0) {
-            lmps.push({
-              lmpType: aliasToLMPType[node.expression.name.text],
-              config: node.arguments[0].getText(sourceFile),
-              fn: node.arguments[1].getText(sourceFile),
-            });
           }
         }
       }
@@ -197,10 +310,11 @@ class EllTSC implements EllTSC {
 
     visit(sourceFile);
 
+    this.lmpCache.set(filePath, lmps);
     return lmps;
   }
 }
 
-new EllTSC()
-  .getLMPsInFile(path.resolve("src/example.ts"))
-  .then(console.log);
+// new EllTSC()
+//   .getLMPsInFile(path.resolve("src/example.ts"))
+//   .then(console.log);
