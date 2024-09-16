8b00fe71ecd7a3ef4afb6f0bd9ffbab6d4e894c8
William Guss <carlostnb@gmail.com>
2024-09-15 19:46:29 -0700

    revert bullshit

--- examples/multilmp.py
+++ examples/multilmp.py
@@ -5,22 +5,22 @@ from ell.stores.sql import SQLiteStore
 
 ell.config.verbose = True
 
-@ell.simple(model="gpt-4o-mini", temperature=1.0)
+@ell.simple(model="gpt-4o-mini", temperature=1.0, max_tokens=15)
 def generate_story_ideas(about : str):
     """You are an expert story ideator. Only answer in a single sentence."""
     return f"Generate a story idea about {about}."
 
-@ell.simple(model="gpt-4o-mini", temperature=1.0)
+@ell.simple(model="gpt-4o-mini", temperature=1.0, max_tokens=15)
 def write_a_draft_of_a_story(idea : str):
     """You are an adept story writer. The story should only be 3 paragraphs."""
     return f"Write a story about {idea}."
 
-@ell.simple(model="gpt-4o", temperature=0.1)
+@ell.simple(model="gpt-4o", temperature=0.1, max_tokens=15)
 def choose_the_best_draft(drafts : List[str]):
     """You are an expert fiction editor."""
     return f"Choose the best draft from the following list: {'\n'.join(drafts)}."
 
-@ell.simple(model="gpt-4-turbo", temperature=0.2)
+@ell.simple(model="gpt-4-turbo", temperature=0.2, max_tokens=15)
 def write_a_really_good_story(about : str):
     ideas = generate_story_ideas(about, lm_params=(dict(n=4)))
 
