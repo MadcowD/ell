1029f32c53bdffafbfee476335604f7d741ec6e9
William Guss <carlostnb@gmail.com>
2024-09-22 17:44:29 -0700

    fix import

--- src/ell/providers/__init__.py
+++ src/ell/providers/__init__.py
@@ -1,7 +1,6 @@
 import ell.providers.openai
-import ell.providers.openai_like
+import ell.providers.groq
 import ell.providers.anthropic
-# import ell.providers.groq
 # import ell.providers.mistral
 # import ell.providers.cohere
 # import ell.providers.gemini
