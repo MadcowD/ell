b954f2046ef183ba0d283f90825e583f22f23848
Frank Hu <100752018+tianyaohu@users.noreply.github.com>
2024-09-22 19:11:17 -0700

    ollama example fix: model name change, register arg fix

--- examples/providers/ollama_ex.py
+++ examples/providers/ollama_ex.py
@@ -2,19 +2,22 @@
 Ollama example.
 """
 import ell
-import openai
 
+ell.init(verbose=True, store='./logdir')
 # Use models automatically registered by asking ollama 
-ell.models.ollama.register(api_base="http://localhost:11434/v1")
-@ell.simple(model="llama3", temperature=0.1)
+ell.models.ollama.register(base_url="http://localhost:11434/v1")
+
+# in terminal run ollama list to see available models
+@ell.simple(model="llama3.1:latest", temperature=0.1)
 def write_a_story():
     return "write me a story"
 
-
-# Or use the client firectly
+# Or use the client directly
+import openai
 client = openai.Client(
     base_url="http://localhost:11434/v1", api_key="ollama"  # required but not used
 )
-@ell.simple(model="llama3", temperature=0.1, max_tokens=10, client=client)
+@ell.simple(model="llama3.1:latest", temperature=0.1, max_tokens=100, client=client)
 def write_a_story_with_client():
-    return "write me a short"
+    return "write me a short story"
+
