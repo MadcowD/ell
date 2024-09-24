48b7351e915501b777bc01e866caabbac13ad3fe
Isac Arnekvist <isac.arnekvist@violet.ai>
2024-09-24 11:02:52 +0200

    multiple tool responses #230

--- tests/test_openai_provider.py
+++ tests/test_openai_provider.py
@@ -1,9 +1,10 @@
+import json
 import pydantic
 import pytest
 from unittest.mock import MagicMock, patch
 from ell.providers.openai import OpenAIProvider, _content_block_to_openai_format
 from ell.provider import EllCallParams
-from ell.types import Message, ContentBlock, ToolCall
+from ell.types import Message, ContentBlock, ToolCall, ToolResult
 from openai import Client
 from openai.types.chat import (
     ChatCompletion,
@@ -159,6 +160,56 @@ class TestOpenAIProvider:
                 "role": "assistant",
                 "content": None,
             }
+        ]
+
+    def test_translate_to_provider_with_multiple_tool_calls(
+        self, provider, ell_call_params, mock_tool
+    ):
+        tool_result_1 = ToolResult(
+            tool_call_id="123",
+            result=[ContentBlock(text="Hello World 1")]
+        )
+        tool_result_2 = ToolResult(
+            tool_call_id="456",
+            result=[ContentBlock(text="Hello World 2")]
+        )
+        message = Message(role="tool", content=[tool_result_1, tool_result_2])
+        ell_call_params.messages = [message]
+        translated = provider.translate_to_provider(ell_call_params)
+        assert translated["messages"] == [
+            {
+                "content": json.dumps(["Hello World 1"]),
+                "role": "tool",
+                "tool_call_id": "123"
+            },
+            {
+                "content": json.dumps(["Hello World 2"]),
+                "role": "tool",
+                "tool_call_id": "456"
+            }
+        ]
+
+    def test_translate_to_provider_with_list_tool_response(
+        self, provider, ell_call_params, mock_tool
+    ):
+        choices = [
+            "Banana",
+            "Apple",
+            "Orange"
+        ]
+        tool_result = ToolResult(
+            tool_call_id="123",
+            result=[ContentBlock(text=choice) for choice in choices]
+        )
+        message = Message(role="tool", tool_result=tool_result)
+        ell_call_params.messages = [message]
+        translated = provider.translate_to_provider(ell_call_params)
+        assert translated["messages"] == [
+            {
+                "content": json.dumps(choices),
+                "role": "tool",
+                "tool_call_id": "123"
+            },
         ]
 
     def test_translate_from_provider_streaming(
