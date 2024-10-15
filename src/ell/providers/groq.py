7d462f746a0d32d7061e649972fc48b88e02bb2a
Reece Holmdahl <accounts@r.holmdahl.io>
2024-10-15 03:24:03 -0500

    Response formatting working consistently * based JSON response beta on Groq docs * tried with many ell examples and variations, all worked * had to find a hacky way to persist the response_format model in the provider using context vars, is thread safe tho. * NOTE: currently breaks Groq tool use, incomplete but I have a fix in the works

--- src/ell/providers/groq.py
+++ src/ell/providers/groq.py
@@ -2,9 +2,17 @@
 Groq provider.
 """
 
+import json
+import contextvars
+
+from pydantic import BaseModel
 from ell.providers.openai import OpenAIProvider
 from ell.configurator import register_provider
+from ell.types.message import ContentBlock, Message
 
+
+# Create a context variable to hold the response_format model between to_provider and from_provider
+store_response_format = contextvars.ContextVar('response_format', default=None)
 
 try:
     import groq
@@ -13,18 +21,53 @@ try:
         def translate_to_provider(self, *args, **kwargs):
             params = super().translate_to_provider(*args, **kwargs)
             params.pop('stream_options', None)
-            assert 'response_format' not in params, 'Groq does not support response_format.'
             params['messages'] = messages_to_groq_message_format(params['messages'])
+
+            # assert 'response_format' not in params, 'Groq does not support response_format.'
+            # Store the response_format model between to_provider and from_provider
+            response_format = params.get('response_format')
+            store_response_format.set(response_format)
+            if isinstance(response_format, type) and issubclass(response_format, BaseModel):
+                # Groq beta JSON response does not support streaming or stop tokens
+                params.pop('stream', None)
+                params.pop('stop', None)
+                params['response_format'] = {'type': 'json_object'}
+                # Groq suggests explain how to respond with JSON in system prompt
+                params['messages'] = add_json_schema_to_system_prompt(response_format, params['messages'])
+
             return params
         
         def translate_from_provider(self, *args, **kwargs):
             res, meta = super().translate_from_provider(*args, **kwargs)
             if not meta['usage']:
                 meta['usage'] = meta['x_groq']['usage']
+
+            response_format = store_response_format.get()
+            if isinstance(response_format, type) and issubclass(response_format, BaseModel):
+                json_text = res[0].content[0].text
+                try:
+                    parsed = response_format(**json.loads(json_text))
+                    res = Message(role='assistant', content=ContentBlock(parsed=parsed))
+                except Exception as e:
+                    raise Exception(f"Could not parse Groq response: {json_text} into {response_format.__name__}") from e
+
             return res, meta
     register_provider(GroqProvider(), groq.Client)
 except ImportError:
     pass
+
+def add_json_schema_to_system_prompt(response_format: BaseModel, messages):
+    json_prompt = f'\n\nYou must respond with a JSON object compliant with following JSON schema:\n{json.dumps(response_format.model_json_schema(), indent=4)}'
+    
+    system_prompt = next(filter(lambda m: m['role'] == 'system', messages), None)
+
+    if system_prompt is None:
+        messages = [{'role': 'system', 'content': json_prompt}] + messages
+    else:
+        system_prompt['content'] += json_prompt
+
+    return messages
+
 
 def messages_to_groq_message_format(messages):
     """Assistant messages to Groq must take the format: {'role': 'assistant', 'content': <string>}"""
