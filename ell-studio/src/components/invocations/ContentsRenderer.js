362565d418111b464f0c03ac090be9151fe60a2b
William Guss <carlostnb@gmail.com>
2024-11-02 15:09:58 -0700

    fix null renderer bug

--- ell-studio/src/components/invocations/ContentsRenderer.js
+++ ell-studio/src/components/invocations/ContentsRenderer.js
@@ -4,6 +4,7 @@ import IORenderer from '../IORenderer';
 
 export function ContentsRenderer({ item, field, ...rest }) {
     const contents = item.contents;
+    console.log(contents[field]);
 
     if (contents.is_external && !contents.is_external_loaded) {
       return <div>Loading...</div>;
