b600afc9732190b2c06e6856e2f51a0e15c80f2e
Aryaman Pandya <91023037+apandy02@users.noreply.github.com>
2024-12-18 16:30:19 -0500

    Update custom install docs link in ell ImportError (#409)

--- src/ell/studio/__init__.py
+++ src/ell/studio/__init__.py
@@ -3,4 +3,4 @@ try:
     import ell.stores
 except ImportError:
     raise ImportError(
-        'ell.studio is missing dependencies. Install them with `pip install -U "ell-ai[studio]"`. More info: https://docs.ell.so/installation/custom-installation')
+        'ell.studio is missing dependencies. Install them with `pip install -U "ell-ai[studio]"`. More info: https://docs.ell.so/installation#custom-installation')
